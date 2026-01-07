5b8280d5908e2cd258b597afa73e2cea1c635b87
Driton Gashi <dritongashi1995@gmail.com>
2026-01-08 00:22:59 +0100

    vercel fix

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 // Vercel serverless function for Express API
 // This file is in /api/index.ts to be recognized by Vercel as a serverless function
 // All requests to /api/* will be handled by this Express app
-import app from '../server/src/index';
+// Note: Server must be built first (npm run build:server) before this will work
+import app from '../server/dist/index';
 
 export default app;
